5760d0405f0b4b7a5ee6b42f6159de2dfb5cb54e
ciaranbrohan <ciaranbrohan.dev@protonmail.com>
2021-03-24 08:36:53 +0000

    flow control statements

---
+++ vampire.js
@@ -0,0 +1,15 @@
+let name = "carol";
+let age = 3000;
+
+if (name === "Alice") {
+	console.log("hello, Alice");
+}
+else if (age < 12 ) {
+	console.log("you're not alice kid!") 
+}
+else if (age > 2000) {
+	console.log("unlike you Alice is not an imortal vampire")
+}
+else if (age > 100 ) {
+	console.log("Alice is not a that old, grannie")
+}
---
+++ vampire2-bad-else-ordering.js
@@ -0,0 +1,17 @@
+let name = "carol";
+let age = 3000;
+
+if (name === "Alice") {
+        console.log("hello, Alice");
+}
+else if (age < 12 ) {
+        console.log("you're not alice kid!") 
+}
+else if (age > 100 ) {
+        console.log("Alice is not a that old, grannie")
+}
+else if (age > 2000) {
+        console.log("unlike you Alice is not an imortal vampire")
+}
+
+
