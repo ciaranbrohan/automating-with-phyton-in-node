e21921b9094538181f4f26cc5eba910119b59cf4
ciaranbrohan <ciaranbrohan.dev@protonmail.com>
2021-03-25 09:07:47 +0000

    five times and simple maths

---
+++ fiveTimes.js
@@ -0,0 +1,5 @@
+console.log('My name is');
+
+for (i = 0; i<5; i++) {
+	console.log(` Slim Shady (${i})`);
+}
---
+++ simpleMaths.js
@@ -0,0 +1,9 @@
+let total = 0;
+
+for (num=1; num<101; num++)
+{
+	total = total + num;
+}
+
+console.log(total);
+
